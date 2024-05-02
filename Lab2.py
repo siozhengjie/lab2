ff23c2c53c7ea4f7d159902ebe3b41a93e1513f5
siozhengjie <ZHENGJIE06.23@ichat.sp.edu.sg>
2024-05-02 14:53:15 +0800

    Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,2 +1,35 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-      
+def main():
+print('This is the main entry point')
+if __name__ == '__main__':
+main()
+
+
+def main():
+ print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+ display_main_menu()
+ num_list = get_user_input()
+
+def display_main_menu():
+ print('display_main_menu')
+ return
+
+def get_user_input():
+ print('get_user_input')
+ return
+
+def calc_average():
+ print('calc_average')
+ return
+
+def find_min_max():
+ print('find_min_max')
+ return
+
+
+def sort_temperature():
+ print('sort_temperature')
+ return
+
+def calc_median_temperature():
+ print('calc_median_temperature')
+ return
--- bmi.py
+++ bmi.py
@@ -1,8 +1,26 @@
 def calculate_bmi(height,weight):
-    print("Height="+ str(height))
-    print("Weight="+str(weight))
+    print("Height=",str(height))
+    print("Weight=",str(weight))
     bmi = weight/pow(height,2)
-    return bmi
+    if bmi < 18.5:
+        return "-1"
+    elif 18.5 <= bmi <= 25.0:
+        return "0"
+    else:
+        return "1"
+    
+        
+
+ 
+
+def display_main_menu():
+    print('Enter some numbers separated by commas') 
+    return
+
+def get_user_input():
+    result = input()
+    return result
 
 print(calculate_bmi(weight=57, height=1.73))
+
 
